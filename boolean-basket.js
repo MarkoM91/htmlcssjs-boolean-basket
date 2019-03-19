6fa1211aded9bc0d17be98148e22e3b023802f1f
MarkoM91 <markomedic91@gmail.com>
2019-03-19 16:32:38 +0100

    commit10px;

--- boolean-basket.js
+++ boolean-basket.js
@@ -1,144 +1,102 @@
-
-
-
 function getPlayerbyId(id, players){
-
-var player;
-   for (var i = 0; i < players.length; i++) {
-     if (players[i].id == id) {
-      player = players[i];
-     }
-   }
-   console.log(player);
-return player;
-
-
+    var player;
+    for (var i = 0; i < players.length; i++) {
+        if (players[i].id == id) {
+            player = players[i];
+        }
+    }
+    console.log(player);
+    return player;
 }
-
 
 function  getRandomPlayers() {
-
-
-
     $.ajax({
-      url : "https://www.boolean.careers/api/array/basket?n=17",
-      method : "GET",
-      success: function(data, state) {
-
-       if (data.success) {
-
-         var players = data.response;
-         updateUI(players)
-       } else {
-
-         alert("Errore");
-       }
-
-
-
-
-    },
-    error: function(request, state, error) {
-      console.log("request", request)
-      console.log("state", state)
-      console.log("error", error)
-    }
-
-  });
-
-return players;
+        url : "https://www.boolean.careers/api/array/basket?n=17",
+        method : "GET",
+        success: function(data, state) {
+            if (data.success) {
+                var players = data.response;
+                updateUI(players)
+            } else {
+                alert("Errore");
+            }
+        },
+        error: function(request, state, error) {
+            console.log("request", request)
+            console.log("state", state)
+            console.log("error", error)
+        }
+    });
+    return players;
 }
 
-
-
 function updateUI(players) {
-
-var datalist = $('#players');
-
-
-  for (var i = 0; i < players.length; i++) {
-
-
-
-    option =document.createElement("option");
-    option.value = players[i].id;
-
-
-    datalist.append(option);
-
-  }
-
-
+    var datalist = $('#players');
+    for (var i = 0; i < players.length; i++) {
+        var option = document.createElement("option");
+        option.value = players[i].playerCode;
+        option.innerHTML =  players[i].playerCode;
+        datalist.append(option);
+    }
 }
 
 function clearClick() {
-  var input = $('#usr-input');
-  input.val("");
+    var input = $('#usr-input');
+    input.val("");
 
-  idDOM =$('#id');
-  idPoints = $('#points > span.content');
-  idBounce = $('#bounce > span.content');
-  idMistake = $('#mistake > span.content');
-  idTwoPerc = $('#twoPerc > span.content');
-  idThreePerc = $('#threePerc > span.content');
+    idDOM =$('#id');
+    idPoints = $('#points > span.content');
+    idBounce = $('#bounce > span.content');
+    idMistake = $('#mistake > span.content');
+    idTwoPerc = $('#twoPerc > span.content');
+    idThreePerc = $('#threePerc > span.content');
 
-  idDOM.text(player.id);
-  idPoints.text(player.points);
-  idBounce.text(player.bounce);
-  idMistake.text(player.mistake);
-  idTwoPerc.text(player.twoPerc);
-  idThreePerc.text(player.threePerc);
+    idDOM.text(player.id);
+    idPoints.text(player.points);
+    idBounce.text(player.bounce);
+    idMistake.text(player.mistake);
+    idTwoPerc.text(player.twoPerc);
+    idThreePerc.text(player.threePerc);
 }
 
 function playerSelection(players, me) {
+    var pickedId = me.val();
+    var player = getPlayerbyId(pickedId, players);
 
- var pickedId = me.val();
+    idDOM =$('#id > span.content');
+    idPoints = $('#points > span.content');
+    idBounce = $('#bounce > span.content');
+    idMistake = $('#mistake > span.content');
+    idTwoPerc = $('#twoPerc > span.content');
+    idThreePerc = $('#threePerc > span.content');
 
- var player = getPlayerbyId(pickedId, players);
-
- idDOM =$('#id > span.content');
- idPoints = $('#points > span.content');
- idBounce = $('#bounce > span.content');
- idMistake = $('#mistake > span.content');
- idTwoPerc = $('#twoPerc > span.content');
- idThreePerc = $('#threePerc > span.content');
-
- idDOM.text(player.id);
- idPoints.text(player.points);
- idBounce.text(player.bounce);
- idMistake.text(player.mistake);
- idTwoPerc.text(player.twoPerc);
- idThreePerc.text(player.threePerc);
+    idDOM.text(player.id);
+    idPoints.text(player.points);
+    idBounce.text(player.bounce);
+    idMistake.text(player.mistake);
+    idTwoPerc.text(player.twoPerc);
+    idThreePerc.text(player.threePerc);
 }
 
 
 function sidebarShow() {
-
-  var sidebar = $(".sidebar");
-
-  sidebar.fadeIn();
-
+    var sidebar = $(".sidebar");
+    sidebar.toggleClass("active");
 }
 
 
 function init() {
+    getRandomPlayers();
+    var clearButton = $('#clear-btn');
+    clearButton.click(clearClick);
 
-
-var players = getRandomPlayers();
-updateUI(players);
-var clearButton = $('#clear-btn');
-clearButton.click(clearClick);
-
-var input = $('#usr-input');
-input.on("change" , function() {
-
-var me = $(this);
-playerSelection(players, me);
-
-var sidebar = $(".sidebar");
- sidebar.click(sidebarShow);
-
-});
+    var input = $('#usr-input');
+    input.on("change" , function() {
+        var me = $(this);
+        playerSelection(players, me);
+        var sidebar = $(".sidebar");
+        sidebar.click(sidebarShow);
+    });
 }
 
 
